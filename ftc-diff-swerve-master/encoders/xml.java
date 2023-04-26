c57fd68ba68a0438115fee03ad2b3e06966e8f3c
roboticsjacky <jacky690764@brisbanegrammar.com>
2023-04-26 15:51:56 +1000

    change

---
+++ ftc-diff-swerve-master/encoders/xml.java
@@ -0,0 +1,8 @@
+package encoders;
+
+public class xml {
+    <?xml version="1.0" encoding="UTF-8"?>
+<project version="4">
+  <component name="Encoding" addBOMForNewFiles="with NO BOM" />
+</project>
+}
